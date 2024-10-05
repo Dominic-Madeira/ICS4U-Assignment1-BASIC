3137b565ec1e921a0658a71f0f1541134de974c5
Debian <dominic.madeira@mths.ca>
2024-10-05 03:17:51 +0000

    started linting

--- MUGWUMP.ts
+++ MUGWUMP.ts
@@ -11,7 +11,7 @@ import { createPrompt } from 'bun-promptx'
 const gridSize = 10
 const numMugwumps = 4
 const maxTries = 10
-let mugwumps: number[][] = new Array(numMugwumps).fill(0).map(() => [0, 0])
+const mugwumps: number[][] = new Array(numMugwumps).fill(0).map(() => [0, 0])
 
 console.log('THE OBJECT OF THIS GAME IS TO FIND FOUR MUGWUMPS')
 console.log('HIDDEN ON A 10 BY 10 GRID. HOMEBASE IS POSITION 0,0')
@@ -25,59 +25,59 @@ console.log('YOU HOW FAR YOU ARE FROM EACH MUGWUMP.')
 console.log()
 
 do {
-    playGame()
-    console.log("THAT WAS FUN! LET'S PLAY AGAIN...")
-    console.log('FOUR NEW MUGWUMPS ARE NOW IN HIDING.')
+  playGame()
+  console.log("THAT WAS FUN! LET'S PLAY AGAIN...")
+  console.log('FOUR NEW MUGWUMPS ARE NOW IN HIDING.')
 } while (true)
 
-function playGame() {
-    placeMugwumps()
-    let tries = 0
+function playGame (): void {
+  placeMugwumps()
+  let tries = 0
 
-    while (tries < maxTries) {
-        tries++
-        console.log()
-        console.log(`TURN NO. ${tries} WHAT IS YOUR GUESS?`)
-        let xCoordinate = createPrompt('WHAT IS THE X COORDINATE? ')
-        let yCoordinate = createPrompt('WHAT IS THE Y COORDINATE? ')
-        xCoordinate = parseInt(xCoordinate.value)
-        yCoordinate = parseInt(yCoordinate.value)
-
-        let foundAll = true
-
-        for (let counter = 0; counter < numMugwumps; counter++) {
-            if (mugwumps[counter][0] == -1) {
-                continue
-            }
-            if (mugwumps[counter][0] == xCoordinate && mugwumps[counter][1] == yCoordinate) {
-                mugwumps[counter][0] = -1
-                console.log(`YOU HAVE FOUND MUGWUMP ${counter + 1}`)
-            } else {
-                let distance = Math.sqrt(Math.pow(mugwumps[counter][0] - xCoordinate, 2) + Math.pow(mugwumps[counter][1] - yCoordinate, 2))
-                console.log(`YOU ARE ${distance.toFixed(1)} UNITS FROM MUGWUMP ${counter + 1}`)
-                foundAll = false
-            }
-        }
-
-        if (foundAll) {
-            console.log()
-            console.log(`YOU GOT THEM ALL IN ${tries} TURNS!`)
-            return
-        }
-    }
-
+  while (tries < maxTries) {
+    tries++
     console.log()
-    console.log("SORRY, THAT'S 10 TRIES. HERE IS WHERE THEY'RE HIDING:")
+    console.log(`TURN NO. ${tries} WHAT IS YOUR GUESS?`)
+    let xCoordinate = createPrompt('WHAT IS THE X COORDINATE? ')
+    let yCoordinate = createPrompt('WHAT IS THE Y COORDINATE? ')
+    xCoordinate = parseInt(xCoordinate.value)
+    yCoordinate = parseInt(yCoordinate.value)
+
+    let foundAll = true
+
     for (let counter = 0; counter < numMugwumps; counter++) {
-        if (mugwumps[counter][0] != -1) {
-            console.log(`MUGWUMP ${counter + 1} IS AT ( ${mugwumps[counter][0]} , ${mugwumps[counter][1]} )`)
-        }
+      if (mugwumps[counter][0] === -1) {
+        continue
+      }
+      if (mugwumps[counter][0] === xCoordinate && mugwumps[counter][1] === yCoordinate) {
+        mugwumps[counter][0] = -1
+        console.log(`YOU HAVE FOUND MUGWUMP ${counter + 1}`)
+      } else {
+        const distance = Math.sqrt(Math.pow(mugwumps[counter][0] - xCoordinate, 2) + Math.pow(mugwumps[counter][1] - yCoordinate, 2))
+        console.log(`YOU ARE ${distance.toFixed(1)} UNITS FROM MUGWUMP ${counter + 1}`)
+        foundAll = false
+      }
     }
+
+    if (foundAll) {
+      console.log()
+      console.log(`YOU GOT THEM ALL IN ${tries} TURNS!`)
+      return
+    }
+  }
+
+  console.log()
+  console.log("SORRY, THAT'S 10 TRIES. HERE IS WHERE THEY'RE HIDING:")
+  for (let counter = 0; counter < numMugwumps; counter++) {
+    if (mugwumps[counter][0] !== -1) {
+      console.log(`MUGWUMP ${counter + 1} IS AT ( ${mugwumps[counter][0]} , ${mugwumps[counter][1]} )`)
+    }
+  }
 }
 
-function placeMugwumps() {
-    for (let counter = 0; counter < numMugwumps; counter++) {
-        mugwumps[counter][0] = Math.floor(Math.random() * gridSize);
-        mugwumps[counter][1] = Math.floor(Math.random() * gridSize);
-    }
+function placeMugwumps (): void {
+  for (let counter = 0; counter < numMugwumps; counter++) {
+    mugwumps[counter][0] = Math.floor(Math.random() * gridSize)
+    mugwumps[counter][1] = Math.floor(Math.random() * gridSize)
+  }
 }
